0cba3dec5f6b5759e53fbb7400693028424f1776
Tiana Lopez <tianadavin@gmail.com>
2023-11-15 09:24:01 -0500

    finishes lab

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -10,5 +10,19 @@ class GetPrograms:
     response = requests.get(URL)
     return response.content
 
-programs = GetPrograms().get_programs()
-print(programs)
+  def program_agencies(self):
+      # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
+
+        return programs_list
+
+# programs = GetPrograms().get_programs()
+# print(programs)
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+for agency in set(agencies):
+    print(agency)
